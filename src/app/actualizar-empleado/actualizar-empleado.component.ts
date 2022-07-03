dc30565d34f91f6a86e5d72fab30c1fe2c7fb202
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2022-07-02 23:37:39 -0400

    Creada funcionalidad de Actualizar Empleado

--- src/app/actualizar-empleado/actualizar-empleado.component.ts
+++ src/app/actualizar-empleado/actualizar-empleado.component.ts
@@ -1,4 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VirtualTimeScheduler } from 'rxjs';
+import { Empleado } from '../empleado';
+import { EmpleadoService } from '../empleado.service';
 
 @Component({
   selector: 'app-actualizar-empleado',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActualizarEmpleadoComponent implements OnInit {
 
-  constructor() { }
+  private id: number;
+  public empleado: Empleado;
+
+  constructor(
+    private empleadoService: EmpleadoService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.empleado = new Empleado();
+    this.obtenerId();
   }
 
+  private obtenerId(): void {
+    this.obtenerDetallesEmpleado();
+  }
+
+  private obtenerDetallesEmpleado(): void {
+    this.empleadoService.obtenerEmpleadoPorId(this.id).subscribe(empleado => {
+      this.empleado = empleado;
+    });
+  }
+
+  public onSubmit(): void {
+    this.actualizarEmpleado();
+  }
+
+  private actualizarEmpleado(): void {
+    this.empleadoService.modificarEmpleadoPorId(this.id, this.empleado).subscribe(dato => {
+      console.log(dato);
+      this.router.navigate(['lista-empleados']);
+    });
+  }
+
+  public volver(): void {
+    this.location.back();
+  }
 }
--- src/app/empleado.service.ts
+++ src/app/empleado.service.ts
@@ -1,3 +1,4 @@
+import { NumberSymbol } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
@@ -25,8 +26,13 @@ export class EmpleadoService {
   }
 
   /* Obtener empleado por ID */
-  obtenerEmpleadoPorId(id: number): Observable<Object> {
-    return this.http.get(`${this.baseUrl}/${id}`);
+  obtenerEmpleadoPorId(id: number): Observable<Empleado> {
+    return this.http.get<Empleado>(`${this.baseUrl}/${id}`);
+  }
+
+  /* Modificar empleado por ID */
+  modificarEmpleadoPorId(id: number, empleado: Empleado): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, empleado);
   }
 
   /* Eliminar empleado por ID */
